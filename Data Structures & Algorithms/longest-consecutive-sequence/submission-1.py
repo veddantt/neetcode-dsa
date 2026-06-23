660bc488b748a4a4cab38292070c94a84a63d7de
Vedant <148513054+veddantt@users.noreply.github.com>
2026-06-23 10:51:43 -0400

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        d = {}
+        occured = {}
+        m = 1 if nums else 0
+        for n in nums:
+          if n not in occured:
+            occured[n]=1
+            if n-1 not in d and n+1 not in d:
+                d[n]=[n, n]
+            else:
+                if n-1 in d:
+                    d[n]=[d[n-1][0], n]
+                    d[d[n-1][0]]= d[n]
+                    m = max(m, d[n][1]-d[n][0]+1 )
+                    if n+1 in d:
+                       new_range = [d[n-1][0], d[n+1][1]]
+                       d[d[n+1][1]]= new_range
+                       d[d[n-1][0]] = new_range
+                       m = max(m, new_range[1]-new_range[0]+1 )
+                else:
+                    d[n] = [n, d[n+1][1]]
+                    d[d[n+1][1]]= d[n]
+                    m = max(m, d[n][1]-d[n][0]+1 )
+        return m
