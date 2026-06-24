8bbf040ec328ccf98672254053d0e0e45245f8e7
Vedant <148513054+veddantt@users.noreply.github.com>
2026-06-24 11:08:13 -0400

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def levelOrder(self, root):
+        if not root:
+            return []
+        ans, level = [], [root]
+        while level:
+            ans.append([node.val for node in level])
+            temp = []
+            for node in level:
+                temp.extend([node.left, node.right])
+            level = [leaf for leaf in temp if leaf]
+        return ans
