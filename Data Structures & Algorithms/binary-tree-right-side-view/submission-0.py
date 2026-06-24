5856e3211dd253e6e1e1310a14a91b06bb153842
Vedant <148513054+veddantt@users.noreply.github.com>
2026-06-24 11:08:55 -0400

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def rightSideView(self, root):
+        if not root:
+            return []
+        right = self.rightSideView(root.right)
+        left = self.rightSideView(root.left)
+        return [root.val] + right + left[len(right):]
+        
